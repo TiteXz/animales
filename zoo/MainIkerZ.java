ef467b1a7c8d7f76019315f7ac1b27ed67cad563
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2022-12-13 14:27:28 +0100

    3 tetra de aletra sangrante creados

--- zoo/MainIkerZ.java
+++ zoo/MainIkerZ.java
@@ -4,6 +4,27 @@ public class MainIkerZ {
 		
 		TetraDeAletaSangrante tetri = new TetraDeAletaSangrante();
 
+		tetri.setNombre("Oliver Atom");
+		tetri.setTamaño(45);
+		tetri.setPesoGr(36);
+		tetri.setEdadmeses(14);
+		tetri.setCantAletas(3);
+		
+		TetraDeAletaSangrante tetri2 = new TetraDeAletaSangrante();
+
+		tetri2.setNombre("Benji Atom");
+		tetri2.setTamaño(47);
+		tetri2.setPesoGr(32);
+		tetri2.setEdadmeses(15);
+		tetri2.setCantAletas(4);
+		
+		TetraDeAletaSangrante tetri3 = new TetraDeAletaSangrante();
+
+		tetri3.setNombre("JaVI Atom");
+		tetri3.setTamaño(52);
+		tetri3.setPesoGr(46);
+		tetri3.setEdadmeses(10);
+		tetri3.setCantAletas(3);
 	}
 
 }
